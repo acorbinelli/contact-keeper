d718613894932ca3d19e97089c4c3d77a5275074
Alessandro Corbinelli <acorbinelli@gmail.com>
2021-05-20 12:49:54 +0300

    create contact form and contact context. add functionality to delete and edit contact

---
+++ client/src/context/contact/ContactState.jsx
@@ -0,0 +1,69 @@
+import React, { useReducer } from "react"
+import { v4 as uuid } from "uuid"
+import ContactContext from "./contactContext"
+import contactReducer from "./contactReducer"
+
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types"
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      { id: 1, name: "John Honk", phone: "111-111-1111", type: "professional" },
+      { id: 2, name: "Mary Slime", phone: "222-222-2222", type: "personal" },
+      { id: 3, name: "Jack Jackson", phone: "333-333-3333", type: "personal" },
+      { id: 4, name: "Jill Kook", phone: "444-444-4444", type: "professional" },
+    ],
+    current: null,
+  }
+
+  const [state, dispatch] = useReducer(contactReducer, initialState)
+
+  //add contact
+  const addContact = (contact) => {
+    contact.id = uuid()
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id })
+  }
+  //delete contact
+
+  //set current contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact })
+  }
+  //clear current contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT })
+  }
+  //update contact
+
+  //filter contacts
+
+  //clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  )
+}
+
+export default ContactState
